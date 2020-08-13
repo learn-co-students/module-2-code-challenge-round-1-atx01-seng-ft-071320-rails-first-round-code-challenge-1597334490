4e5438614e24f2513bf4cfd775df587b6a317832
Philip Roush <roush.philip@gmail.com>
2020-08-13 14:02:43 -0500

    submission for deadline

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,24 @@
 class PowersController < ApplicationController
+  before_action :get_that_power, only:[:show]
+
   def index
     @powers = Power.all
   end
+
+  def show 
+  end
+
+
+  
+
+  private
+
+  def get_that_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :heroine_ids)
+  end
+
 end
